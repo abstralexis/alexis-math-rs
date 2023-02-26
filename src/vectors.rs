d8aa199bd31e29596d2c8a8d82338ea57b8136c4
AlexisComix <Thecomixguy@hotmail.com>
2023-02-26 16:33:47 +0000

    Add dot product function

--- src/vectors.rs
+++ src/vectors.rs
@@ -9,6 +9,15 @@ impl Vec2 {
         Vec2 {x, y}
     }
     
+    fn __clone_xydeg(vec2: &Vec2, degrees: &f32) -> (f32, f32, f32) {
+        /* A private method to get x, y, and degree value clones */
+        (
+            (*vec2).x.clone(),
+            (*vec2).y.clone(),
+            (*degrees).clone()
+        )
+    }
+    
     pub fn rotate(&mut self, degrees: &f32) {
         /* Rotate the vector by degrees degrees */
         
@@ -18,15 +27,6 @@ impl Vec2 {
         // Set values to rotated
         self.x = (x * deg.to_radians().cos()) - (y * deg.to_radians().sin());
         self.y = (x * deg.to_radians().sin()) + (y * deg.to_radians().cos());
-    }
-
-    fn __clone_xydeg(vec2: &Vec2, degrees: &f32) -> (f32, f32, f32) {
-        /* A private method to get x, y, and degree value clones */
-        (
-            (*vec2).x.clone(),
-            (*vec2).y.clone(),
-            (*degrees).clone()
-        )
     }
     
     pub fn from_rotate(vec2: &Vec2, degrees: &f32) -> Vec2 {
@@ -38,5 +38,15 @@ impl Vec2 {
             x: (x * deg.to_radians().cos()) - (y * deg.to_radians().sin()),
             y: (x * deg.to_radians().sin()) + (y * deg.to_radians().cos())
         }
+    }
+    
+    pub fn dot(&self, vec2: &Vec2) -> f32 {
+        /* Returns the dot product of self and another Vec2 */
+        (self.x * (*vec2).x) + (self.y * (*vec2).y)
+    }
+    
+    pub fn dot_product(v1: &Vec2, v2: &Vec2) -> f32 {
+        /* Returns the dot product of two Vec2s */
+        ((*v1).x * (*v2).y) + ((*v1).y * (*v2).y)
     }
 } 
